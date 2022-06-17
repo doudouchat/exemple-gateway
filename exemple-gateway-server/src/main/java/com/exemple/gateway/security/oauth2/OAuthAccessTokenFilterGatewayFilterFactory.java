c8c1bb2acef646b0541d37a6ca642a744da4bbaa
doudouchat <baraultb@hotmail.com>
2022-06-17 09:59:41 +0100

    FIX POST auth/token 401

--- exemple-gateway-server/src/main/java/com/exemple/gateway/security/oauth2/OAuthAccessTokenFilterGatewayFilterFactory.java
+++ exemple-gateway-server/src/main/java/com/exemple/gateway/security/oauth2/OAuthAccessTokenFilterGatewayFilterFactory.java
@@ -67,7 +67,7 @@ public class OAuthAccessTokenFilterGatewayFilterFactory extends AbstractGatewayF
                         saveSession(exchange, previousBody);
                     }
 
-                    return Mono.just(previousBody);
+                    return Mono.justOrEmpty(previousBody);
                 });
     }
 

091e1f7a1450306dd6b59ac6e193c1e03d2deac5
doudouchat <baraultb@hotmail.com>
2023-01-20 11:24:30 +0100

    set status 200 if redirection filter

---
+++ exemple-gateway-core/src/test/java/com/exemple/gateway/core/redirection/RedirectionFilterTest.java
@@ -0,0 +1,75 @@
+package com.exemple.gateway.core.redirection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockserver.model.Header;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+import com.exemple.gateway.core.GatewayServerTestConfiguration;
+import com.exemple.gateway.core.common.LoggingFilter;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class RedirectionFilterTest extends GatewayServerTestConfiguration {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private RequestSpecification requestSpecification;
+
+    @BeforeEach
+    public void before() {
+
+        requestSpecification = RestAssured.given().filters(new LoggingFilter(LOG));
+        authorizationClient.reset();
+
+    }
+
+    @Test
+    void redirectionMoveToOk() {
+
+        // Given mock client
+        authorizationClient.when(HttpRequest.request().withMethod("POST").withPath("/ExempleAuthorization/login"))
+                .respond(HttpResponse.response()
+                        .withHeaders(new Header("Content-Type", "application/json;charset=UTF-8"),
+                                new Header("Location", "http://localhost:" + this.authorizationClient.getPort() + "/ExempleAuthorization/123"))
+                        .withStatusCode(302));
+
+        // When perform post
+        Response response = requestSpecification.post(restTemplate.getRootUri() + "/ExempleAuthorization/login");
+
+        // Then check response
+        assertAll(
+                () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.getHeader("Location")).isNull());
+
+    }
+    
+    @Test
+    void redirectionMoveToKO() {
+
+        // Given mock client
+        authorizationClient.when(HttpRequest.request().withMethod("POST").withPath("/ExempleAuthorization/login"))
+                .respond(HttpResponse.response()
+                        .withStatusCode(401));
+
+        // When perform post
+        Response response = requestSpecification.post(restTemplate.getRootUri() + "/ExempleAuthorization/login");
+
+        // Then check response
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+
+    }
+
+}
